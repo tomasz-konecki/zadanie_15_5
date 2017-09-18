7b6f2632f2d1f2ee23ccb9b7436f30f3dfc7d9fb
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-18 23:10:03 +0200

    Done

--- components/Gif.js
+++ components/Gif.js
@@ -10,7 +10,7 @@ var GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif',
             return this.props.sourceUrl || GIPHY_LOADING_URL;
         },
         render: function() {
-            var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
+            const url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
     
             return (
                 <div style={styles}>
--- components/Search.js
+++ components/Search.js
@@ -7,7 +7,7 @@ Search = React.createClass({
     },
 
     handleChange: function(event) {
-        var searchingText = event.target.value;
+        const searchingText = event.target.value;
         this.setState({searchingText: searchingText});
     
         if (searchingText.length > 2) {
@@ -22,19 +22,19 @@ Search = React.createClass({
     },
 
     render: function() {
-        var styles = {
-            fontSize: '1.5em',
-            width: '90%',
-            maxWidth: '350px'
+        const styles = {
+              fontSize: '1.5em',
+              width: '90%',
+              maxWidth: '350px'
         };
     
         return <input
-                type="text"
-                onChange={this.handleChange}
-                onKeyUp={this.handleKeyUp}
-                placeholder="Tutaj wpisz wyszukiwaną frazę"
-                style={styles}
-                value={this.state.searchTerm}
+                    type="text"
+                    onChange={this.handleChange}
+                    onKeyUp={this.handleKeyUp}
+                    placeholder="Tutaj wpisz wyszukiwaną frazę"
+                    style={styles}
+                    value={this.state.searchTerm}
                 />
     }
 });
