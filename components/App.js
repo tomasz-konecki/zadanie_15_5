a35f9bb1bd4e681192a846723d7f9f1872249a8c
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-18 22:23:10 +0200

    Update code

--- components/App.js
+++ components/App.js
@@ -20,13 +20,14 @@ var App = React.createClass({
                 searchingText: searchingText
             });
         }.bind(this))
-        .then(responseText => { 
-                var data = JSON.parse(xhr.responseText).data,
-                            gif = {
+        .then(data => { 
+                        var gif = {
                             url: data.fixed_width_downsampled_url,
                             sourceUrl: data.url
                             };
-                        callback(gif);
+                        this.setState(gif: gif);
+
+                        //callback(gif);
         })
         .catch(error => console.log(error));
 
@@ -40,23 +41,25 @@ var App = React.createClass({
                     GIPHY_PUB_KEY = 'hUzWNheMHr7KPeZM3lGHSoUyq5rDSIik',
                     url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText,
                     xhr = new XMLHttpRequest(); 
+
+                xhr.open('GET', url);
+                xhr.send();
         
                 xhr.onload = function() {
                     if (xhr.status === 200) {
-                        resolve(this.responseText);
+                        var data = JSON.parse(xhr.responseText).data;
+                        resolve(data);
                     }
                     else {
-                        reject(new Error(this.statusText));
+                        reject(new Error(this.error));
                     }
                 };
 
                 xhr.onerror = function() {
                     reject(new Error(
-                        `XMLHttpRequest Error: ${this.statusText}`));
+                        `XMLHttpRequest Error: ${this.error}`));
                 };
 
-                xhr.open('GET', url);
-                xhr.send();
         });
     },
 
